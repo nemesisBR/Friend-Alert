de55c388e60c76d7148cf250b2cbc246b44ca069
Boby Robert <bobyrobert3@gmail.com>
2019-03-15 10:36:17 +0530

    Cropping image added
    Added function 'getName' to get current  matched face's name.
    
    'getFIles' now returns names of all files without extension.
    
    'storeImages' prints  the name of persons whose picture was saved.
    
    Added feature : Code iterates through testing images and findsout faes encodings in eah image. It then matches with known_faces_encoding to get a match and also finds the corresponding name.
    
    if recognized name is present then corresponding face is cropped out and stored.

--- create_dataset_face.py
+++ create_dataset_face.py
@@ -28,22 +28,25 @@ def knownFacesCollecting(filenames):
 
 #----Function to store images 
 def storeImages(name, crop_img):
-    cv2.imwrite(current_path + "/face_database/" + name +"/" + str(np.random.randint(1000)) + ".png",crop_img)
-
+    #print("F:\Pictures\Delhi 2015\Photoshop\\" + name + str(np.random.randint(1000)) + ".png")
+    print("Saved:", name)
+    cv2.imwrite("F:\Pictures\Delhi 2015\Photoshop\\" + name + str(np.random.randint(1000)) + ".png",crop_img)
+    #print(current_path + "F:\Pictures\Delhi 2015\Photoshop\\" + name + str(np.random.randint(1000)) + ".png")
 #----Function to return all JPEG file's path in a directory
 def getFiles(path):
     folder = os.fsencode(path)
     filenames = []
+    names = []
     for file in os.listdir(folder):
         filename = os.fsdecode(file)
         if filename.endswith(('.JPG')):
             filenames.append(path + '\\' + str(filename))
-    return filenames
+            names.append(str(filename[0:-4]))
+    return filenames, names
 
 
 #----Variables----
-face_locations = []
-face_encodings = []
+
 face_names = []
 frame_number = 0
 
@@ -53,20 +56,36 @@ counter = 0
 counter1 = 0
 
 #-------Creating list of training filenames of known faces along with path----
-known_faces = getFiles('')
+known_faces , face_names = getFiles('F:\GitHub\Friend Alert\KnownImages')
 known_faces_encoded = knownFacesCollecting(known_faces)
+ 
 
 
 #-------Creating list of training filenames along with path-----
-farewell_images = getFiles('F:\Pictures\BE Photoshoot')
+farewell_images ,file_names = getFiles('F:\Pictures\BE Photoshoot')
 
 #-------Processing Farewell Images--------------
+count = 0
 for path in farewell_images:
-    image = face_recognition.load_image(path)
+    count = count + 1
+    print(count)
+    faceNamesPhoto = []
+    face_locations = []
+    face_encodings = []
+    image = face_recognition.load_image_file(path)
     face_locations = face_recognition.face_locations(image)
-    face_encodings = face_recognition.face_encoding(image,face_locations)
+    face_encodings = face_recognition.face_encodings(image,face_locations)
     
-
-
+    for face_encoding in face_encodings:
+        match = face_recognition.compare_faces(known_faces_encoded,face_encoding, tolerance = 0.50)
+        
+        name = getName(match, face_names)
+        faceNamesPhoto.append(name)
+    
+    for (top, right, bottom, left), name in zip(face_locations, faceNamesPhoto):
+        if not name:
+            continue
+        crop_image = image[top:bottom, left:right]
+        storeImages(name, crop_image)
 
 
